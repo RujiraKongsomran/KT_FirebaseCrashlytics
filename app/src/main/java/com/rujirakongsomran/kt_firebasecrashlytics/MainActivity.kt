87fa9185267d7d722865a52463c5bcca34b13550
RujiraKongsomran <rujirabiwberry@gmail.com>
2021-07-05 15:05:05 +0700

    Force a test crash to finish setup

--- app/src/main/java/com/rujirakongsomran/kt_firebasecrashlytics/MainActivity.kt
+++ app/src/main/java/com/rujirakongsomran/kt_firebasecrashlytics/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.rujirakongsomran.kt_firebasecrashlytics
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.rujirakongsomran.kt_firebasecrashlytics.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val binding: ActivityMainBinding by lazy { ActivityMainBinding.inflate(layoutInflater) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+//        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        binding.button.setOnClickListener {
+            crashNow()
+        }
+    }
+
+    private fun crashNow() {
+        throw RuntimeException("Test Crash") // Force a crash
     }
 }
